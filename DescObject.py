e3bd8285c255c2296679c08b540cccf78dfaac72
Charish <203925512+kazashi1@users.noreply.github.com>
2026-01-15 15:29:46 +0800

    Commit3

---
+++ DescObject.py
@@ -0,0 +1,14 @@
+class Rectangle:
+	def __init__(self, width, height):
+		self.width = width
+		self.height = height
+
+	def calculate_area(self)
+		return self.width * self.height
+
+	def __str__(self):
+		return f"Rectangle: {self.width} x {self.height}"
+
+rectangle = Rectangle(4, 8)
+print(rectangle)
+print("Area:", rectangle.calculate_area())
